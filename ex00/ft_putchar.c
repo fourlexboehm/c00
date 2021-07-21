24525c42004964ce7159b841f75593e32dc40e62
Alex Boehm <alexboehm@systemli.org>
2021-07-21 22:59:34 +0930

    exo1

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-//#include <stdio.h>
-	
+
 void	ft_putchar(char c)
 {
-	/* write takes the fd which is 0 for sdin
-	1 for sdout or 2 for sderr, a constand,
-	and a length */
 	write(1, &c, 1);
 }
 
-/*int	main()
-{
-	ft_putchar("c");
-}*/	
+/* write takes the fd which is 0 for sdin
+1 for sdout or 2 for sderr, a constand,
+and a length */	
+
+/*int	main(){
+	ft_putchar('a');
+}*/
