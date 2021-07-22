81d7b5a078b795f2e33cb200e262930552b04a5f
Alex Boehm <alexboehm@systemli.org>
2021-07-22 11:32:08 +0930

    fixed

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -2,21 +2,29 @@
 
 void	ft_print_comb(void)
 {
-	char a;	
-	char b;
-	char c;
-	a = 0;
-	b = 0;
-	c = 0;
-	while( a < 9 && b < 9 && c < 9 )
-		if 	c == 9 
-				
-			
-	if	a !== 		
-		write(1, &a, 1);
-		write(1, &b, 1);
-		write(1, &c, 1);
-	}	
+	int a;
+	int b;
+	int c;
+	
+	a = '0';
+	b = '1';
+	c = '2';
+
+	while( a <= '7' ){
+		while ( b <= '8' ){
+			b++;
+			while ( c <= '9' ){
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
+
+				if	( a != 7 || a != 8 || b != 9 )
+					+
+			}
+			b++;
+		}
+		a++;
+	}
 }
 
 int	main(void)
